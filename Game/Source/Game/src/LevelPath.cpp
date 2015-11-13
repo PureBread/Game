ab395b1f69f5e6e69e3205171a9b3e805cbeda49
Emma Thurlow <ecthurlow@rogers.com>
2015-11-13 14:20:47 -0500

    Trying to simplify line...

--- Game/Source/Game/src/LevelPath.cpp
+++ Game/Source/Game/src/LevelPath.cpp
@@ -14,7 +14,7 @@ LevelPath::LevelPath(std::string _texDir)
 	unsigned int minX = texture->width, maxX = 0, minY = texture->height;
 
 	// find contour borders
-	for (int i = 0; i < vertices.size(); ++i){
+	for (unsigned int i = 0; i < vertices.size(); ++i){
 		glm::vec2 v = vertices.at(i);
 		if (v.x < minX){
 			minX = v.x;
@@ -29,7 +29,7 @@ LevelPath::LevelPath(std::string _texDir)
 
 	// Save the value of the hightest left border vertex
 	int maxY_l = minY;
-	for (int i = 0; i < vertices.size(); ++i){
+	for (unsigned int i = 0; i < vertices.size(); ++i){
 		if (vertices.at(i).x == minX){
 			if (vertices.at(i).y > maxY_l){
 				maxY_l = vertices.at(i).y;
@@ -39,7 +39,7 @@ LevelPath::LevelPath(std::string _texDir)
 
 	// Save the value of the hightest right border vertex
 	int maxY_r = minY;
-	for (int i = 0; i < vertices.size(); ++i){
+	for (unsigned int i = 0; i < vertices.size(); ++i){
 		if (vertices.at(i).x == maxX){
 			if (vertices.at(i).y > maxY_r){
 				maxY_r = vertices.at(i).y;
@@ -58,6 +58,41 @@ LevelPath::LevelPath(std::string _texDir)
 		}else{
 			++it;
 		}
+	}
+
+	// Simplify path
+	// 0 = constant, 1 = increasing, -1 = decreasing
+	int smooth = 3;
+
+	if (vertices.size() > 2){
+		std::vector<glm::vec2> simplified;
+		simplified.push_back(vertices.front());
+
+		int slope = vertices.at(1).y - vertices.at(0).y > 0 ? 1 : vertices.at(1).y - vertices.at(0).y < 0 ? 1 : 0;
+
+		for (int i = 1; i < vertices.size(); ++i){
+			glm::vec2 v = vertices.at(i);
+			glm::vec2 prev = vertices.at(i - 1);
+
+			int deltaY = v.y - prev.y;
+			int prevSlope = slope;
+
+			if (deltaY == 0 && slope != 0){
+				slope = 0;
+			}else if (deltaY > 0 && slope != 1){
+				slope = 1;
+			}else if (deltaY < 0 && slope != -1){
+				slope = -1;
+			}
+			
+			// If slope direction changes at this point, keep the vertex, else remove it
+			if (prevSlope != slope){
+				simplified.push_back(prev);
+			}
+		}
+
+		simplified.push_back(vertices.back());
+		vertices = simplified;
 	}
 
 	int blah = 0;
