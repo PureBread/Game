5be3e1149ac7919847b895c134cce6a502c46e62
seleb <sean.s.leblanc@gmail.com>
2015-11-14 13:51:23 -0500

    missing file from last commit

--- Game/Source/Game/src/PlayerManager.cpp
+++ Game/Source/Game/src/PlayerManager.cpp
@@ -5,16 +5,27 @@
 #include <FileUtils.h>
 #include <Log.h>
 #include <Step.h>
+#include <NumberUtils.h>
+#include <MY_ResourceManager.h>
+
+Event::Event(EventType _type, Scenario * _scenario) :
+	type(_type),
+	scenario(_scenario)
+{
+}
 
 PlayerManager::PlayerManager() :
 	momentDelay(0.5f),
-	momentTimer(0)
+	momentTimer(0),
+	eventToTrigger(nullptr)
 {
 	loadDefaults();
 }
 
 PlayerManager::~PlayerManager(){
-
+	if(eventToTrigger != nullptr){
+		delete eventToTrigger;
+	}
 }
 
 void PlayerManager::save(const std::string & _path){
@@ -71,8 +82,76 @@ void PlayerManager::moment(){
 	}
 	statistics["health"] += (statistics["rations"]-1)*1.5f - (statistics["speed"] - 1);
 
-	// TODO: check for events
-	//randomEventCheck();
-	//lossEventCheck();
-	//destinationEventCheck();
+	// check for events
+	if(shouldTriggerDestinationEvent()){
+		eventToTrigger = triggerDestinationEvent();
+	}else if(shouldTriggerLossEvent()){
+		eventToTrigger = triggerLossEvent();
+	}else if(shouldTriggerRandomEvent()){
+		eventToTrigger = triggerRandomEvent();
+	}
+}
+
+
+bool PlayerManager::shouldTriggerDestinationEvent(){
+	// check progress against markers
+	return false;
+}
+
+bool PlayerManager::shouldTriggerLossEvent(){
+	// random check which is more likely to succeed when health is low
+	return sweet::NumberUtils::randomFloat(0, 100) < (5 * (100 - statistics["health"])/100);
+}
+
+bool PlayerManager::shouldTriggerRandomEvent(){
+	// completely random check
+	return sweet::NumberUtils::randomFloat(0, 100) < 5;
+}
+
+Event * PlayerManager::triggerDestinationEvent(){
+	return new Event(kDESTINATION, MY_ResourceManager::scenario);
+}
+
+Event * PlayerManager::triggerLossEvent(){
+	bool eventValid = true;
+	unsigned long int selectedEvent;
+	Scenario * scenario;
+	do{
+		// get random event
+		selectedEvent = sweet::NumberUtils::randomInt(0, MY_ResourceManager::lossEvents.size()-1);
+		scenario = MY_ResourceManager::lossEvents.at(selectedEvent);
+		// check if event is valid
+	}while(!eventValid); // while random event 
+
+	// if the event is non-repeatable, remove it from the active event list
+	if(false){
+		MY_ResourceManager::lossEvents.erase(MY_ResourceManager::lossEvents.begin()+selectedEvent);
+	}
+	
+	return new Event(kLOSS, scenario);
+}
+
+Event * PlayerManager::triggerRandomEvent(){
+	bool eventValid = true;
+	unsigned long int selectedEvent;
+	Scenario * scenario;
+	do{
+		// get random event
+		selectedEvent = sweet::NumberUtils::randomInt(0, MY_ResourceManager::randomEvents.size()-1);
+		scenario = MY_ResourceManager::randomEvents.at(selectedEvent);
+		// check if event is valid
+	}while(!eventValid); // while random event 
+
+	// if the event is non-repeatable, remove it from the active event list
+	if(false){
+		MY_ResourceManager::randomEvents.erase(MY_ResourceManager::randomEvents.begin()+selectedEvent);
+	}
+
+	return new Event(kRANDOM, scenario);
+}
+
+Event * PlayerManager::consumeEvent(){
+	Event * res = eventToTrigger;
+	eventToTrigger = nullptr;
+	return res;
 }
